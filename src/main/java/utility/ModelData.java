8a825b68ab9b98ff430dc26c495f8378f1423757
Nilsiker <nilsiker94@gmail.com>
2020-06-08 13:32:43 +0200

    collada parser break out to separate project

--- src/main/java/utility/ModelData.java
+++
@@ -1,8 +0,0 @@
-package utility;
-
-public interface ModelData {
-    float[] getVertexPositions();
-    float[] getNormals();
-    float[] getTexCoords();
-    // TODO getters for animations, materials etc.
-}
